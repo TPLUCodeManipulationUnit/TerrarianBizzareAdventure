ff8246240d42c6c82294864ac16868bee924d0b5
webmilio <therealwebmilio@gmail.com>
2019-08-31 19:24:24 -0400

    Updated set of Type Extensions

--- Extensions/TypeExtensions.cs
+++ Extensions/TypeExtensions.cs
@@ -6,18 +6,24 @@ namespace TerrarianBizzareAdventure.Extensions
 {
     public static class TypeExtensions
     {
-        public static string GetTexturePathFromType(this Type type)
+        public static string GetTexturePath(this Type type)
         {
             string[] segments = type.Namespace.Split('.');
             return string.Join("/", segments, 1, segments.Length - 1) + '/' + type.Name;
         }
 
+        public static string GetTexturePathRoot(this Type type)
+        {
+            string[] segments = type.Namespace.Split('.');
+            return string.Join("/", segments, 1, segments.Length - 1);
+        }
+
         /// <summary>Finds the appropriate texture based solely on the type and its associated mod.</summary>
         /// <param name="type"></param>
         /// <returns></returns>
-        public static Texture2D GetTexture(this Type type) => type.GetModFromType().GetTexture(type.GetTexturePathFromType());
+        public static Texture2D GetTexture(this Type type) => type.GetModFromType().GetTexture(type.GetTexturePath());
 
-        public static Texture2D GetTexture(this Mod mod, Type type) => mod.GetTexture(type.GetTexturePathFromType());
+        public static Texture2D GetTexture(this Mod mod, Type type) => mod.GetTexture(type.GetTexturePath());
 
         public static Texture2D GetTexture(this Type type, Mod mod) => mod.GetTexture(type);
 
